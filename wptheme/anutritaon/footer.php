c377fb009b3d4ddce85d4f5bf78169d06e75f665
Christian Herber Santos <christianfaldox@gmail.com>
2023-12-14 08:33:33 -0400

    add btn back top top ao tema

--- wptheme/anutritaon/footer.php
+++ wptheme/anutritaon/footer.php
@@ -1,3 +1,11 @@
+    <a href="#block_01" class="backto-top shadow rounded">
+        <img 
+            src="img/up-arrow.png"
+            alt="Voltar ao Topo"
+            width="16"
+            height="16">
+    </a>
+
 </body>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 </html>
